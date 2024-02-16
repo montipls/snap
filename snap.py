7334d3142d07688d23aadee62017e6b6ab164016
montipls <montikoszo@gmail.com>
2024-02-16 23:41:04 +0000

    Stable

--- snap.py
+++ snap.py
@@ -1,12 +1,37 @@
 import json
 
 
-def get_names() -> list:
-    names = input('Please enter names: \
-        [name1, name2, name3]\n... ')
+VERSION = 'v1.1'
+AUTHOR = 'Monti'
+NAME = 'Snassist'
 
-    # turns it into a list of names
-    return names.split(', ')
+
+def print_info() -> None:
+    print(f'{NAME} {VERSION} by {AUTHOR}\n')
+
+
+def get_names() -> list:
+    amount = input('2/[3] players: ')
+    if amount == '2':
+        amount = 2
+    else:
+        amount = 3
+
+    players = []
+    for i in range(amount):
+        player = input(f'Name of [player{i+1}]: ')
+        # default name for players
+        if player == '':
+            player = f'player{i+1}'
+        players.append(player)
+    return players
+
+
+def ask_autosave() -> bool:
+    inp = input('Autosave progress? [y]/n: ')
+    if inp.lower() in ['n', 'no']:
+        return False
+    return True
 
 
 def get_points(nam: list, his: list) -> dict:
@@ -21,17 +46,23 @@ def get_points(nam: list, his: list) -> dict:
 
 
 # global variables
+print_info()
 names = get_names()
+autosave = ask_autosave()
+savable = False
 history = []
 inform = True
 winners = {}
 won = False
+
+print('\uf45b'*54)
 
 # main loop
 while True:
     round_ = len(history)
     caller = round_ % len(names)
     points = get_points(names, history)
+    savable = False
 
     # checking for winners
     if not won:
@@ -49,12 +80,13 @@ while True:
 
     # info prompt
     if inform and not won:
-        print(f'\nRound {round_ + 1}!')
-        print(f'Caller is {names[caller]}!\n')
+        print(f'Round {round_ + 1}!')
+        print(f'Caller is {names[caller]}!')
         inform = False
 
     # taking user input
-    cmd = input('... ')
+    cmd = input(f'\n@> ')
+    print('\uf45b'*54)
     # skipping empty command
     if cmd == '':
         inform = True
@@ -77,12 +109,13 @@ while True:
                 if i != caller:
                     result[n] = int(cmd[-1])
             history.append(result)
+        savable = True
 
     # managing commands
     elif cmd in ['quit', 'q', 'exit']:
         exit()
 
-    elif cmd == 'save':
+    if cmd == 'save' or (autosave and savable):
         save_data = json.dumps({
             'names': names,
             'history': history,
@@ -90,9 +123,8 @@ while True:
         # writing data to json file
         with open('game.json', 'w') as f:
             f.write(save_data)
-        print('Game data saved!\n')
 
-    elif cmd == 'load':
+    if cmd == 'load':
         inform = True
         # reading data from json file
         with open('game.json', 'r') as f:
@@ -100,34 +132,41 @@ while True:
         # loading the data
         names = save_data['names']
         history = save_data['history']
-        print('Game data loaded!')
+        print('Loaded from latest save!\n')
 
     elif cmd in ['points', 'pp', 'standings']:
-        print('\nCurrent standings:')
+        print('Standings:')
         # displaying points
         sort = sorted(points.items(), key=lambda x: x[1], reverse=True)
         for i, e in enumerate(sort):
             n, p = e
-            print(f'{"I"*(i+1)}. {n}: {p} pts')
-        print()
+            print(f'  {"I"*(i+1)}. {n}: {p} pts')
 
     elif cmd == 'history':
-        print('\nGame history:')
+        if len(history) < 1:
+            print('No history...')
+            continue
+        print('History:')
         # displaying game history
         for r in history:
             n = ', '.join(list(r.keys()))
             p = list(r.values())[0]
-            print(f'* {p} pts -> {n}')
-        print()
+            print(f'  {p} pts -> {n}')
 
     elif cmd in ['undo', 'back']:
+        if len(history) < 1:
+            print('Nothing to undo...')
+            continue
         history.pop()
-        print('Undo successful!\n')
+        print('Undo successful!')
 
     elif cmd == 'new':
         # resets everything
         names = get_names()
+        autosave = ask_autosave()
+        savable = False
         history = []
         inform = True
         winners = {}
         won = False
+        print('\uf45b'*54)
